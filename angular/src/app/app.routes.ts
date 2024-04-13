c6993b9e8b9931bfbf3ac3ecf5f1aad354ce5047
ritesh-1997 <riteshg763@gmail.com>
2024-04-13 09:49:50 +0530

    removed componnent

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,13 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { TransactComponent } from './transact/transact.component';
-import { HoldingsComponent } from './holdings/holdings.component';
-import { HomepageComponent } from './homepage/homepage.component';
+
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'transact', component: TransactComponent},
-    {path: 'holdings',component: HoldingsComponent},
-    {path: 'home', component: HomepageComponent}
+
   ];
